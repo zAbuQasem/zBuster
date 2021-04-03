39db71eb99014c799841d1df9bff8a6668c42bee
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2021-04-03 01:38:38 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,15 @@
-#!/bin/sh
-#For apt
-#crackmapexec
-sudo python3 -m pip install pipx
-sudo pipx ensurepath
-sudo apt-get install python3-venv
-pipx install crackmapexec
+#!/bin/bash
 
-#feroxbuster
-curl -sLO https://github.com/epi052/feroxbuster/releases/latest/download/feroxbuster_amd64.deb.zip
-unzip feroxbuster_amd64.deb.zip
-sudo apt install ./feroxbuster_*_amd64.deb
-
-#seclist
-cd /usr/share
-sudo wget -c https://github.com/danielmiessler/SecLists/archive/master.zip -O SecList.zip \
-  && sudo unzip SecList.zip \
-  && sudo rm -f SecList.zip
+w=$(echo $UID)
+if [[ "$w" != "0" ]]; then
+	echo -e "\e[31mROOT PERMISSIONS REQUIRED!\e[0m"
+else
+	#for crackmapexec
+	python3 -m pip install pipx
+	pipx ensurepath
+	apt-get install python3-venv
+	pipx install crackmapexec
+	crackmapexec
+	#for nfs
+	apt install nfs-common
+fi
