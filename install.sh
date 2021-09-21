73ca3c30195d854513df015938a118545dea7b0a
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2021-09-21 21:31:19 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,15 @@ w=$(echo $UID)
 if [[ "$w" != "0" ]]; then
 	echo -e "\e[31mROOT PERMISSIONS REQUIRED!\e[0m"
 else
+	wget https://golang.org/dl/go1.16.7.linux-amd64.tar.gz
+	sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.16.7.linux-amd64.tar.gz
+	mv go1.16.7.linux-amd64.tar.gz DElETE_ME/
+	echo 'export GOPATH=$HOME/go' >> $HOME/.bashrc
+	echo 'export GOPATH=$HOME/go' >> $HOME/.zshrc
+	echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bashrc
+	echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.zshrc
+	export GOPATH=$HOME/go
+	export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
 	#for crackmapexec
 	python3 -m pip install pipx
 	pipx ensurepath
