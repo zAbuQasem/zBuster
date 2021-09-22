a052ab5569f1cf8d5b5f8dd6dcefa98b98936101
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2021-09-22 18:22:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
-
-w=$(echo $UID)
-if [[ "$w" != "0" ]]; then
-	echo -e "\e[31mROOT PERMISSIONS REQUIRED!\e[0m"
-else
-	wget https://golang.org/dl/go1.16.7.linux-amd64.tar.gz
-	sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.16.7.linux-amd64.tar.gz
-	mv go1.16.7.linux-amd64.tar.gz DElETE_ME/
-	echo 'export GOPATH=$HOME/go' >> $HOME/.bashrc
-	echo 'export GOPATH=$HOME/go' >> $HOME/.zshrc
-	echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bashrc
-	echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.zshrc
-	export GOPATH=$HOME/go
-	export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
-	GO111MODULE=on go get -u github.com/jaeles-project/gospider
-	#for crackmapexec
-	python3 -m pip install pipx
-	pipx ensurepath
-	apt-get install python3-venv
-	pipx install crackmapexec
-	crackmapexec
-	#for nfs
-	apt install nfs-common
-	#memcache
-	apt install libmemcached-tools
-fi
+echo "[++]Installing Golang 1.16.7"
+echo "[!]CTRL+C if you want to abort the installation"
+sleep 6
+wget https://golang.org/dl/go1.16.7.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.16.7.linux-amd64.tar.gz
+mv go1.16.7.linux-amd64.tar.gz DElETE_ME/
+echo 'export GOPATH=$HOME/go' >> $HOME/.bashrc
+echo 'export GOPATH=$HOME/go' >> $HOME/.zshrc
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bashrc
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.zshrc
+export GOPATH=$HOME/go
+export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
+GO111MODULE=on go get -u github.com/jaeles-project/gospider
+#for crackmapexec
+sudo python3 -m pip install pipx
+sudo pipx ensurepath
+sudo apt-get install python3-venv
+sudo pipx install crackmapexec
+crackmapexec
+#for nfs
+sudo apt install nfs-common
+#memcache
+sudo apt install libmemcached-tools
